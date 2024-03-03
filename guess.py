3b08f9fa6fc83ae51a33f79bae04c7de620a2712
Sin <Sin814074@gmail.com>
2024-03-03 22:19:04 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,14 +1,18 @@
 import random
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數字範圍結束值:')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1 #count = count + 1
-	num = input ('請猜數字1~100: ')
+	num = input ('請猜數字: ')
 	num = int(num)
 	if num != r and num < r:
-		print('猜錯囉! 比答案還要小喔!')
+		print('比答案還要小喔!')
 	elif num != r and num > r:
-		print('猜錯囉! 比答案還要大喔!')
+		print('比答案還要大喔!')
 	else:
 		num == r
 		print('終於猜對了!')
